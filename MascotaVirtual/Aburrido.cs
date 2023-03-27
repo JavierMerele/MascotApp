5685450103ec092a0f2a860b096f10c4d617aa88
JavierMerele <javiermereleet12d1@gmail.com>
2023-03-27 15:07:14 -0300

    Aburrido

--- MascotaVirtual/Aburrido.cs
+++ MascotaVirtual/Aburrido.cs
@@ -2,9 +2,19 @@ namespace MascotaVirtual;
 
 public class Aburrido : IEstado
 {
+    public DateTime Inicio { get; private set; }
+
+    public static TimeSpan IntervaloComer { get; set; } = TimeSpan.FromSeconds(8);
+
+    public TimeSpan IntervaloAburrida => DateTime.Now - Inicio;
+
+
+    public Aburrido() => Inicio = DateTime.Now;
+
     public void Comer(Mascota mascota)
     {
-        throw new NotImplementedException();
+        if (IntervaloAburrida > IntervaloComer)
+            mascota.Estado(new Contento());
     }
 
     public void Jugar(Mascota mascota)
